dd84c3064c0a571311af716af972fe76669c8af2
Christian <christianstringham@gmail.com>
2022-06-02 09:54:45 -0600

    mvp

---
+++ src/components/CardDetails.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Button from '@mui/material/Button';
+
+function CardDetails({ onAddToCart, onRemoveFromCart }) {
+    const [card, setCard] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false);
+    let { id } = useParams()
+
+
+    useEffect(() => {
+        fetch(`http://localhost:6001/items/${id}`)
+            .then(res => res.json())
+            .then(resCard => {
+                setCard(() => resCard)
+                setIsLoaded(() => true);
+            })
+    }, [id])
+
+    if (!isLoaded) return <h1>Loading...</h1>;
+
+    const { name, brand, instruFam, rating, price, condition, imageOne, imageTwo, imageThree, inCart } = card;
+
+    function handleClickCart() {
+        inCart ? onRemoveFromCart(card.id) : onAddToCart(card.id);
+        setCard({ ...card, inCart: !inCart })
+    }
+
+    const cartButton = (inCart) ? <Button onClick={handleClickCart}>Remove from cart</Button> : <Button onClick={handleClickCart}>Add to cart</Button>;
+
+    return (
+        <div>
+            {/* image bar + image display to be entered here. add state of selected image */}
+            <div className="info">
+                <h3>{name}</h3>
+                <p>{'Brand: ' + brand}</p>
+                <p>{instruFam}</p>
+                <p>{'Rating: ' + rating}</p>
+                <p>{'Condition: ' + condition}</p>
+                <p>{'Price: ' + price}</p>
+                {cartButton}
+            </div>
+        </div>
+    )
+}
+
+export default CardDetails
