17e4c14c3bcf419fb3d6cc6580cc93667af92a8f
Mai Vang <vmaineng@gmail.com>
2022-06-03 08:56:48 -0700

    dabble

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,14 @@
 import Card from './Card';
 //import { Route, Switch } from "react-router-dom";
-
+import Grid from '@mui/material/Grid'
 
 function CardList({ itemList, onAddToCart }) {
-
-    const cards = itemList.map((card) => <Card key={card.id} card={card} onChangeCart={onAddToCart} source='items' />)
-
+    
+const cards = itemList.map((card) => <Card key={card.id} card={card} onChangeCart={onAddToCart} source='items' />)
     return (
-        <div>
-            <ul id='CardList__Main'>
-                {cards}
-            </ul>
-        </div>
+        <Grid container spacing={2}>
+                 {cards}
+         </Grid>
     )
 }
-
 export default CardList
